1446f95fd4ec386bb8240a7253a6af90070f14f5
alice <wlw330296385@gmail.com>
2017-09-09 17:13:17 +0800

    去掉版本冲突的文件

--- application/api/controller/Frontend.php
+++ application/api/controller/Frontend.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace app\api\controller;
@@ -30,37 +29,4 @@ class Frontend extends Base{
     $this->redirect('index/noLogin');die;
   }
 
-=======
-<?php
-
-namespace app\api\controller;
-
-use app\service\Wechat;
-use app\api\controller\Base;
-use app\service\TokenService;
-
-class Frontend extends Base{
-
-
-
-
-  public function _initialize() {  	
-
-      parent::_initialize();
-      $is_login = cookie('member');
-      $this->memberInfo = session('memberInfo','','think');
-  	   if(($is_login!=md5($this->memberInfo['id'].$this->memberInfo['create_time'].'hot'))||!$is_login||!$this->memberInfo){
-
-    		$this->checklogin();
-    	}
-  }
- 
-
-  public function checklogin(){ 
-
-    // return json(['code'=>200,'msg'=>'请先登录']);die;
-    $this->redirect('index/noLogin');die;
-  }
-
->>>>>>> 537fcbdec4eac0f0cc174cc52c48678313dd5ed2
 }
