7c1078487b5c36f57be3bbae5b2fe16f335e937c
alice <wlw330296385@gmail.com>
2017-09-14 16:18:14 +0800

    ['assistants'] = unserialize(['assistant']);

--- application/frontend/controller/Grade.php
+++ application/frontend/controller/Grade.php
@@ -61,6 +61,9 @@ class Grade extends Base{
     public function updateGrade(){
     	$grade_id = input('param.grade_id');
         $gradeInfo = $this->GradeService->getGradeInfo(['id'=>$grade_id]);
+        if($gradeInfo['assistant']){
+            $gradeInfo['assistants'] = unserialize($gradeInfo['assistant']);
+        }
         // 判读权限
         $CampService = new \app\service\CampService;
         $is_power = $CampService->isPower($gradeInfo['camp_id'],$this->memberInfo['id']);
