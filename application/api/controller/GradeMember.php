4ad580a54ac97dd7201b939d61a32ac37abefa64
wayen <zwr_0320@163.com>
2017-10-23 14:35:36 +0800

    录课和编辑页面学员提交获初始值，和提交表单数字判断。

--- application/api/controller/GradeMember.php
+++ application/api/controller/GradeMember.php
@@ -87,4 +87,52 @@ class GradeMember extends Base{
             return json(['code'=>100,'msg'=>$e->getMessage()]);
         }
     }
+
+
+  
+    // 获取与课程|班级|训练营相关的学生|体验生-带page
+    public function getGradeMemberListByPageApi(){
+        try{
+            $map = input('post.');
+            $keyword = input('param.keyword');
+            if(!empty($keyword)&&$keyword != ' '&&$keyword != ''){
+                $map['student'] = ['LIKE','%'.$keyword.'%'];
+            } 
+            if( isset($map['keyword']) ){
+                unset($map['keyword']);
+            }
+            $result = $this->GradeMemberService->getGradeMemberListByPage($map);    
+            if($result){
+                return json(['code'=>200,'msg'=>'ok','data'=>$result]);
+            }else{
+                return json(['code'=>100,'msg'=>'检查你的参数']);
+            }
+        }catch (Exception $e){
+            return json(['code'=>100,'msg'=>$e->getMessage()]);
+        }
+    }
+    // 获取与课程|班级|训练营相关的学生|体验生-不带page
+    public function getGradeMemberListNoPageApi(){
+        try{
+            $map = input('post.');
+            $keyword = input('param.keyword');
+            if(!empty($keyword)&&$keyword != ' '&&$keyword != ''){
+                $map['student'] = ['LIKE','%'.$keyword.'%'];
+            } 
+            if( isset($map['keyword']) ){
+                unset($map['keyword']);
+            }
+            $GradeMember = new \app\model\GradeMember;
+            $result = $GradeMember->where($map)->select();    
+            if($result){
+                return json(['code'=>200,'msg'=>'ok','data'=>$result]);
+            }else{
+                return json(['code'=>100,'msg'=>'检查你的参数']);
+            }
+        }catch (Exception $e){
+            return json(['code'=>100,'msg'=>$e->getMessage()]);
+        }
+    }
+
+
 }
