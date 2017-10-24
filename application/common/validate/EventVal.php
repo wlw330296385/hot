786031f2fe04787886c8134370b0cf429bf912d1
alice <wlw330296385@gmail.com>
2017-10-24 18:25:54 +0800

    增加eventService和event模型和event验证器

---
+++ application/common/validate/EventVal.php
@@ -0,0 +1,36 @@
+<?php 
+namespace app\common\validate;
+use think\Validate;
+class EventVal extends Validate{
+
+
+	protected $rule = [
+        'event'        =>  'require|max:60',
+        'event_type'	=> 'require',
+        'price'         => 'require',
+        'coach'         =>'require',
+        'week'           =>'require',
+        'event_time'           =>'require',
+        'province'              =>'require',
+        'city'                  =>'require',
+        'area'                  =>'require',
+        'location'                 =>'require',
+    ];
+    
+    protected $message = [
+        'event.require'        =>  '活动名必须',
+        'event_type.require'	=> '活动类型必须',
+        'price.require'      =>'费用必须',
+        'event_time'           =>'发布时间必须大于当前时间',
+        'province'              =>'省份必填',
+        'city'                  =>'城市必填',
+        'area'                  =>'区县必填',
+        'location'                 =>'活动地点必填',
+    ];
+    
+    protected $scene = [
+        'add'   =>  ['event','event_type','event_time','province','city','area','location'],
+        'edit'  =>  [],
+    ];    
+
+}
