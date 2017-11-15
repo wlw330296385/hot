621b03b67e3746d4d860ad45f1c8e38585d692b9
alice <wlw330296385@gmail.com>
2017-11-15 17:42:14 +0800

    新增recruitment验证器规则

--- application/common/validate/RecruitmentVal.php
+++ application/common/validate/RecruitmentVal.php
@@ -6,6 +6,11 @@ class RecruitmentVal extends Validate{
         'recruitment'  =>  'require|max:60|token',
         'member_id'	=> 'require',
         'province' => 'require',
+        'telephone'=> 'require',
+        'organization_id' =>'require',
+        'organization_type'=>'require',
+        'deadline'  =>'require',
+
     ];
     
     protected $message = [
@@ -13,12 +18,15 @@ class RecruitmentVal extends Validate{
         'recruitment.require'  =>  '请填写班级名称',
         'member_id.require'	=> '会员信息过期,请重新登录平台',
         'province.require' => '请选择所属地区',
-        
+        'telephone.require'=> '请选择联系人',
+        'organization_id.require' =>'organization_id.require',
+        'organization_type.require'=>'organization_type.require',
+        'deadline.require'  =>'请填写截止日期',
     ];
     
     protected $scene = [
-        'add'   =>  ['member_id','recruitment','province'],
-        'edit'  =>  ['member_id','province'],
+        'add'   =>  ['member_id','recruitment','province','telephone','organization_id','organization_type','deadline'],
+        'edit'  =>  ['member_id'],
     ];
 
 }
