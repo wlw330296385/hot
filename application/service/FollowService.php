6d1fe4a24702af212a1554e3de39a9cef3879672
weilin <704671079@qq.com>
2017-11-17 15:21:35 +0800

    添加/取消关注、我的关注列表、被关注列表

---
+++ application/service/FollowService.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\service;
+use app\model\Follow;
+
+class FollowService {
+    // 保存follow数据
+    public function saveFollow($data) {
+        $model = new Follow();
+        $hadFollow = $model->where(['type' => $data['type'], 'follow_id' => $data['follow_id'], 'member_id' => $data['member_id']])->find();
+        if (!$hadFollow) {
+            $data['status'] = 1;
+            $result = $model->allowField(true)->save($data);
+            if ($result) {
+                $response = ['code' => 200, 'msg' => '关注成功', 'data' => $model->id];
+            } else {
+                $response = ['code' => 100, 'msg' => __lang('MSG_400')];
+            }
+        } else {
+            $hadFollow = $hadFollow->toArray();
+            $data['id'] = $hadFollow['id'];
+            $msg = '';
+            if ($hadFollow['status']==1) {
+                $data['status'] = -1;
+                $msg = '取消关注成功';
+            } else {
+                $data['status'] = 1;
+                $msg = '关注成功';
+            }
+            $result = $model->allowField(true)->isUpdate()->save($data);
+            if ($result) {
+                $response = ['code' => 200, 'msg' => $msg];
+            } else {
+                $response = ['code' => 100, 'msg' => __lang('MSG_400')];
+            }
+        }
+        return $response;
+    }
+
+    // 关注列表
+    public function getfollowlist($map, $order='id desc', $paginate=10) {
+        $model = new Follow();
+        $list = $model->where($map)->order($order)->paginate($paginate);
+        return $list;
+    }
+}
