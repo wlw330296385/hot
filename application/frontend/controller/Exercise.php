650d3e307def5f5e11dae3c7701a4a433bda4185
alice <wlw330296385@gmail.com>
2017-12-14 15:01:26 +0800

    1

--- application/frontend/controller/Exercise.php
+++ application/frontend/controller/Exercise.php
@@ -19,19 +19,23 @@ class Exercise extends Base{
 
     public function exerciseInfo(){
     	$exercise_id = input('param.exercise_id');
+        $camp_id = input('param.camp_id',0);
     	$exerciseInfo = $this->ExerciseService->getExerciseInfo(['id'=>$exercise_id]);
         $power = $exerciseInfo['member_id'] == $this->memberInfo['id'] ? 1 : 0;
+        $CampService = new CampService();
+        $campInfo = $CampService->getCampInfo($camp_id);
         // 获取类型
         $exercisePInfo =  $this->ExerciseService->getExerciseInfo(['id'=>$exerciseInfo['pid']]);
         $this->assign('exercisePInfo',$exercisePInfo);
         $this->assign('exerciseInfo',$exerciseInfo);
         $this->assign('power', $power);
+        $this->assign('campInfo',$campInfo);
     	return view('Exercise/exerciseInfo');
     }
 
     //创建项目
     public function createExercise(){
-        $camp_id = input('param.camp_id');
+        $camp_id = input('param.camp_id',0);
         $CampService = new CampService();
         $campInfo = $CampService->getCampInfo($camp_id);
         // 分类
@@ -51,13 +55,16 @@ class Exercise extends Base{
         }
 
         // 权限判断
-        $power = $this->ExerciseService->isPower($exerciseInfo);
+        $power = $this->ExerciseService->isPower($exerciseInfo['camp_id'],$this->memberInfo['id']);
         if($power<2){
             $this->error('权限不足');
         }
+        $CampService = new CampService();
+        $campInfo = $CampService->getCampInfo($camp_id);
         // 分类
         $ExerciseType = $this->ExerciseService->getExerciseType();
 
+        $this->assign('campInfo',$campInfo);
         $this->assign('ExerciseType',$ExerciseType);
 		$this->assign('ExerciseInfo',$exerciseInfo);
     	return view('Exercise/updateExercise');
@@ -66,12 +73,15 @@ class Exercise extends Base{
     // 项目列表
     public function exerciseList(){
         $camp_id = input('param.camp_id',0);
+        $CampService = new CampService();
+        $campInfo = $CampService->getCampInfo($camp_id);
+
     	$myExerciseList = $this->ExerciseService->getExerciseList(['camp_id'=>$camp_id]);
         $sysExerciseList = $this->ExerciseService->getExerciseList(['camp_id'=>0]);
         $this->assign('myExerciseList',$myExerciseList);
         $this->assign('sysExerciseList',$sysExerciseList);
         // dump($sysExerciseList);die;
-        $this->assign('camp_id',$camp_id);
+        $this->assign('campInfo',$campInfo);
         return view('Exercise/exerciseList');
     }
 
