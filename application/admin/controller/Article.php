c32da22b547d2c2d0800f2e4f5e0b15dba6a1d7e
1303274598@qq.com <1303274598@qq.com>
2018-01-22 18:24:39 +0800

    卡券详情判断

---
+++ application/admin/controller/Article.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+
+use app\admin\controller\base\Backend;
+
+class Article extends Backend {
+	public function _initialize(){
+		parent::_initialize();
+	}
+    public function artileList() {
+            
+    	
+    }
+}
