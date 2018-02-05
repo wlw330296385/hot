185e3fcfffa49ac45bd136e1ac79d53de768702d
1303274598@qq.com <1303274598@qq.com>
2018-02-05 12:18:39 +0800

    文章手机端前端

--- application/api/controller/Article.php
+++ application/api/controller/Article.php
@@ -1,20 +1,20 @@
 <?php 
 namespace app\api\controller;
 use app\api\controller\Base;
-use app\service\BankcardService;
-class Bankcard extends Base{
-   protected $BankcardService;
+use app\service\ArticleService;
+class Article extends Base{
+   protected $ArticleService;
  
     public function _initialize(){
         parent::_initialize();
-       $this->BankcardService = new BankcardService;
+       $this->ArticleService = new ArticleService;
     }
  
-    public function getBankcardListApi(){
+    public function getArticleListApi(){
          try{
             $map = input('post.');
             $page = input('param.page')?input('param.page'):1; 
-            $result = $this->BankcardService->getBankcardList($map,$page);  
+            $result = $this->ArticleService->getArticleList($map,$page);  
             if($result){
                 return json(['code'=>200,'msg'=>'获取成功','data'=>$result]);
             }else{
@@ -26,25 +26,40 @@ class Bankcard extends Base{
          }
      }
  
-    public function updateBankcardApi(){
+     public function getArticleListByPageApi(){
+         try{
+            $map = input('post.');
+            $result = $this->ArticleService->getArticleListByPage($map);  
+            if($result){
+                return json(['code'=>200,'msg'=>'获取成功','data'=>$result]);
+            }else{
+                return json(['code'=>100,'msg'=>'无数据']);
+            }  
+            
+         }catch (Exception $e){
+             return json(['code'=>100,'msg'=>$e->getMessage()]);
+         }
+     }
+
+    public function updateArticleApi(){
          try{
              $data = input('post.');
             $bankcard_id = input('param.bankcard_id');
              $data['member_id'] = $this->memberInfo['id'];
              $data['member'] = $this->memberInfo['member'];
-            $result = $this->BankcardService->updateBankcard($data,$bankcard_id);
+            $result = $this->ArticleService->updateArticle($data,$bankcard_id);
              return json($result);
          }catch (Exception $e){
              return json(['code'=>100,'msg'=>$e->getMessage()]);
          }
      }
  
-    public function createBankcardApi(){
+    public function createArticleApi(){
          try{
              $data = input('post.');
              $data['member_id'] = $this->memberInfo['id'];
              $data['member'] = $this->memberInfo['member'];
-            $result = $this->BankcardService->createBankcard($data);
+            $result = $this->ArticleService->createArticle($data);
              return json($result);   
          }catch (Exception $e){
              return json(['code'=>100,'msg'=>$e->getMessage()]);
