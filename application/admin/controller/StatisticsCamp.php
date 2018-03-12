1027f43da772e854428341bd609d9b509c9450a0
1303274598@qq.com <1303274598@qq.com>
2018-03-12 17:49:36 +0800

    1

--- application/admin/controller/StatisticsCamp.php
+++ application/admin/controller/StatisticsCamp.php
@@ -107,6 +107,7 @@ class StatisticsCamp extends Backend{
             
             //课时收入
             $income3 = db('income')->field("sum('income') as s_income,from_unixtime(create_time,'%Y%m%d') as days,sum('schedule_income') as s_schedule_income")->where(['camp_id'=>$camp_id,'type'=>3])->where(['create_time'=>['between',[$month_start,$month_end]]])->where('delete_time',null)->group('days')->select();
+            // dump($income3);die;  
             //课时薪资
             $schedule =  db('schedule')->field("sum((coach_salary+assistant_salary+salary_base)*students) as s_s_salary,from_unixtime(finish_settle_time,'%Y%m%d') as days,sum(cost*students*schedule_rebate) as s_s_rebate,is_settle,camp_id")->where(['camp_id'=>$camp_id,'is_settle'=>1])->where(['finish_settle_time'=>['between',[$month_start,$month_end]]])->where('delete_time',null)->group('days')->select();
 
@@ -437,12 +438,12 @@ class StatisticsCamp extends Backend{
         $month_start = strtotime($monthStart);
         $month_end = strtotime($monthEnd);
 
-        $schedulList = db('schedule')
+        $scheduleList = db('schedule')
         ->where(['lesson_id'=>$lesson_id,'is_settle'=>1])
         ->where(['create_time'=>['between',[$month_start,$month_end]]])
         ->select();
-
-        $this->assign('schedulList',$schedulList);
+        // dump($scheduleList);die;
+        $this->assign('scheduleList',$scheduleList);
 
         return view('StatisticsCamp/lessonSchedule');
     }
