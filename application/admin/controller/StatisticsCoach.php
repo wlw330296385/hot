139f4b0ebc62f330e94527481a814c11bfa21d4a
1303274598@qq.com <1303274598@qq.com>
2018-03-21 15:41:42 +0800

    收益统计

--- application/admin/controller/StatisticsCoach.php
+++ application/admin/controller/StatisticsCoach.php
@@ -60,26 +60,35 @@ class StatisticsCoach extends Backend{
 
     // 收益统计
     public function coachIncome(){
-        $member_id = input('member_id',19);
-        $monthStart = input('param.monthstart',date('Ymd',strtotime('-1 month', strtotime("first day of this month"))));
-        $monthEnd = input('param.monthend',date('Ymd'));
-        $salaryinList = db('salary_in')->field('*,sum(salary) as s_salary,sum(push_salary) as s_push_salary,sum(students) as s_students,count(id) as c_id')->where(['member_id'=>$member_id])->group('lesson_id')->select();
+        $member_id = input('member_id');
         $c_id = 0;
         $s_salary = 0;
         $s_students = 0;
         $s_push_salary = 0;
-        foreach ($salaryinList as $key => $value) {
-            $c_id += $value['c_id'];
-            $s_salary = $value['s_salary'];
-            $s_students = $value['s_students'];
-            $s_push_salary = $value['s_push_salary']; 
-        }
-
-        $rebateList = db('rebate')->where(['member_id'=>2])->select();
         $s_rebate = 0;
-        foreach ($rebateList as $key => $value) {
-            $s_rebate += $value['salary'];
+        $salaryinList = [];
+        $rebateList = [];
+        if($member_id){
+            $yearmonth = date('Ym')-1;
+            $monthStart = input('param.monthstart',date('Ymd',strtotime('-1 month', strtotime("first day of this month"))));
+            $monthEnd = input('param.monthend',date('Ymd'));
+            $month_start = strtotime($monthStart);
+            $month_end = strtotime($monthEnd)+86399;
+            $salaryinList = db('salary_in')->field('*,sum(salary) as s_salary,sum(push_salary) as s_push_salary,sum(students) as s_students,count(id) as c_id')->where(['member_id'=>$member_id,'schedule_time'=>['between',[$month_start,$month_end]]])->group('lesson_id')->select();
+            foreach ($salaryinList as $key => $value) {
+                $c_id += $value['c_id'];
+                $s_salary = $value['s_salary'];
+                $s_students = $value['s_students'];
+                $s_push_salary = $value['s_push_salary']; 
+            }
+
+            $rebateList = db('rebate')->where(['member_id'=>$member_id,'datemonth'=>$yearmonth])->select();
+            
+            foreach ($rebateList as $key => $value) {
+                $s_rebate += $value['salary'];
+            }
         }
+        
         $this->assign('c_id',$c_id);
         $this->assign('s_salary',$s_salary);
         $this->assign('s_students',$s_students);
