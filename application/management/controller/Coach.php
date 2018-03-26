c5a53c6b431974882b40cc1cae9aefad1a31411f
1303274598@qq.com <1303274598@qq.com>
2018-03-26 10:01:19 +0800

    2

--- application/management/controller/Coach.php
+++ application/management/controller/Coach.php
@@ -3,9 +3,7 @@ namespace app\management\controller;
 use app\management\controller\Backend;
 // 按课时结算的训练营财务页面
 class Coach extends Backend{
-	public $camp_member;
 	public function _initialize(){
 		parent::_initialize();
-        $this->camp_member = session('camp_mmeber');
 	}
 }
