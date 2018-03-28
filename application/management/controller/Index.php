3e1eba660c5bcc43c2f0df1b5df0374959df5fd2
1303274598@qq.com <1303274598@qq.com>
2018-03-28 15:33:41 +0800

    雪云分部

--- application/management/controller/Index.php
+++ application/management/controller/Index.php
@@ -21,10 +21,12 @@ class Index extends Backend
 		// 一个月的收益
 		$monthIncome = db('income')->where(['camp_id'=>$this->camp_member['camp_id']])->whereTime('create_time','m')->where('delete_time',null)->sum('income');
 		if($campInfo['rebate_type'] == 2){
-			$monthOutput = db('output')->where(['camp_id'=>$this->camp_member['camp_id'],''])->whereTime('create_time','m')->where('delete_time',null)->sum('output');
+			$monthOutput = db('output')->where(['camp_id'=>$this->camp_member['camp_id']])->where('type',
+				'not in',[-1,-2,3])->whereTime('create_time','m')->where('delete_time',null)->sum('output');
 			$monthOutput = $monthOutput?$monthOutput:0;
 			$monthIncome = $monthIncome - $monthOutput;
-		}		// 总收益
+		}		
+		// 总收益
 		$totalIncome = db('income')->where(['camp_id'=>$this->camp_member['camp_id']])->where('delete_time',null)->sum('income');
 		// 赠课总人数
 		$totalGift = db('schedule_giftrecord')->where(['camp_id'=>$this->camp_member['camp_id']])->where('delete_time',null)->sum('student_num');
@@ -32,6 +34,8 @@ class Index extends Backend
 		$totalStudents = db('schedule')->where(['camp_id'=>$this->camp_member['camp_id']])->where('delete_time',null)->sum('students');
 		//总课时
 		$totalSchedule = db('schedule')->where(['camp_id'=>$this->camp_member['camp_id']])->where('delete_time',null)->count('id');
+
+
 
 		// 培训购买总量曲线图
 		$billList = db('bill')->field("sum(balance_pay) as s_balance_pay,from_unixtime(create_time,'%Y%m%d') as days,goods_type")->where(['camp_id'=>$this->camp_member['camp_id'],'is_pay'=>1])->whereTime('create_time','m')->group('days')->select();
@@ -58,6 +62,16 @@ class Index extends Backend
 			}
 		}
 
+
+
+		// 教学点分布
+		$gradeCourt = db('grade')->field('sum(students) as s_students,court')->where(['status'=>1])->group('court_id')->select();
+		$gradeCourtData = [];
+		foreach ($gradeCourt as $key => $value) {
+			$gradeCourtData['legend'][] = $value['court'];
+			$gradeCourtData['series'][] = ['value'=>$value['s_students'],'name'=>$value['court']];
+		}
+		$this->assign('gradeCourtData',json_encode($gradeCourtData,true));
 		$this->assign('lessonBill',json_encode($lessonBill));
 		$this->assign('eventBill',json_encode($eventBill));
 		$this->assign('monthIncome',$monthIncome);
