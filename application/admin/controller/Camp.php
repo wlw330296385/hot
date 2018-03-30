dfb6a9e5a203db94214ba9de87d3035d777e4ab9
1303274598@qq.com <1303274598@qq.com>
2018-03-30 16:19:48 +0800

    1

--- application/admin/controller/Camp.php
+++ application/admin/controller/Camp.php
@@ -63,16 +63,16 @@ class Camp extends Backend {
             }
             // 保存课时收入平台抽取比例空值
             if (empty($request['schedule_rebate'])) {
-                $request['schedule_rebate'] = null;
+                $request['schedule_rebate'] = 0.1;
             }
             $execute = Db::name('camp')->update($request);
 
             $Auth = new AuthService();
             if ( $execute || ($execute === 0) ) {
-                $Auth->record('训练营id:'. $id .' 修改信息 成功');
+                $Auth->record("训练营id:{$id} 修改信息 成功 data:{json_encode($request)}");
                 $this->success(__lang('MSG_200'), 'camp/index');
             } else {
-                $Auth->record('训练营id:'. $id .' 修改信息 失败');
+                $Auth->record("训练营id:{$id} 修改信息 失败 data:{json_encode($request)}");
                 $this->error(__lang('MSG_400'));
             }
         }
