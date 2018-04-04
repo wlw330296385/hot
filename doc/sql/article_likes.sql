fe324e03760f0daa30efd3657edab8cf8486c7e8
1303274598@qq.com <1303274598@qq.com>
2018-04-04 10:47:58 +0800

    fixed

--- doc/sql/article_likes.sql
+++ doc/sql/article_likes.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50553
  File Encoding         : 65001
 
- Date: 03/04/2018 16:25:13
+ Date: 04/04/2018 10:47:50
 */
 
 SET NAMES utf8mb4;
@@ -25,7 +25,9 @@ CREATE TABLE `article_likes`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
   `article` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `likes` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否点赞1:点赞|-1:取消点赞',
+  `member_id` int(11) NOT NULL,
+  `member` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否点赞1:点赞|-1:取消点赞',
   `create_time` int(11) NOT NULL,
   `update_time` int(11) NOT NULL,
   `delete_time` int(11) NULL DEFAULT NULL,
