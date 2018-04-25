3750d4855aac8f1b48513018b3db1b8222a59e7a
weilin <704671079@qq.com>
2018-04-25 18:09:43 +0800

    球员技术数据 字段名规范调整

--- application/api/controller/Matchdata.php
+++ application/api/controller/Matchdata.php
@@ -45,7 +45,7 @@ class Matchdata
             // 平均数据
             $avgdata = [];
             $avgdata = $model->where($data)
-                ->field('round(avg(pts),1) as pts, round(avg(ast),1) as ast, round(avg(reb),1) as reb, round(avg(stl),1) as stl, round(avg(blk),1) as blk, round(avg(turnover),1) as turnover, round(avg(foul),1) as foul, round(avg(fg),1) as fg, round(avg(fga),1) as fga, round(avg(3pfg),1) as 3pfg, round(avg(3pfga),1) as 3pfga, round(avg(ft),1) as ft, round(avg(fta),1) as fta')
+                ->field('round(avg(pts),1) as pts, round(avg(ast),1) as ast, round(avg(reb),1) as reb, round(avg(stl),1) as stl, round(avg(blk),1) as blk, round(avg(turnover),1) as turnover, round(avg(foul),1) as foul, round(avg(fg),1) as fg, round(avg(fga),1) as fga, round(avg(threepfg),1) as threepfg, round(avg(threepfga),1) as threepfga, round(avg(ft),1) as ft, round(avg(fta),1) as fta')
                 ->find();
             if ($avgdata) {
                 $avgdata = $avgdata->toArray();
@@ -56,28 +56,28 @@ class Matchdata
             $avgFgHitRate = ( $avgdata['fga'] ) ? $avgdata['fg']/$avgdata['fga'] : 0;
             $avgdata['fg_hitrate'] = round($avgFgHitRate*100,1).'%';
             // 平均3分命中率
-            $avgFg3pHitRate = ( $avgdata['3pfga'] ) ? $avgdata['3pfg']/$avgdata['3pfga'] : 0;
-            $avgdata['3pfg_hitrate'] = round($avgFg3pHitRate*100, 1).'%';
+            $avgFg3pHitRate = ( $avgdata['threepfga'] ) ? $avgdata['threepfg']/$avgdata['threepfga'] : 0;
+            $avgdata['threepfg_hitrate'] = round($avgFg3pHitRate*100, 1).'%';
             // 平均罚球命中率
             $avgFtHitRate = ( $avgdata['fta'] ) ? $avgdata['ft']/$avgdata['fta'] : 0;
             $avgdata['ft_hitrate'] = round($avgFtHitRate*100, 1).'%';
             // 平均命中率(综合2分与3分）
-            $avgHitRate = ($avgdata['fga'] && $avgdata['3pfga']) ? ($avgdata['fg']+$avgdata['3pfg'])/($avgdata['fga']+$avgdata['3pfga']) : 0;
+            $avgHitRate = ($avgdata['fga'] && $avgdata['threepfga']) ? ($avgdata['fg']+$avgdata['threepfg'])/($avgdata['fga']+$avgdata['threepfga']) : 0;
             $avgdata['hitrate'] = round($avgHitRate*100, 1).'%';
             // 数据总和
             $sumdata = [];
             $efficiency = 0;
             $sumdata = $model->where($data)
-                ->field('sum(pts) as pts, sum(ast) as ast, sum(reb) as reb, sum(stl) as stl, sum(blk) as blk, sum(turnover) as turnover, sum(foul) as foul, sum(fg) as fg, sum(fga) as fga, sum(fg) as fg, sum(3pfg) as 3pfg, sum(3pfga) as 3pfga, sum(ft) as ft, sum(fta) as fta')
+                ->field('sum(pts) as pts, sum(ast) as ast, sum(reb) as reb, sum(stl) as stl, sum(blk) as blk, sum(turnover) as turnover, sum(foul) as foul, sum(fg) as fg, sum(fga) as fga, sum(fg) as fg, sum(threepfg) as threepfg, sum(threepfga) as threepfga, sum(ft) as ft, sum(fta) as fta')
                 ->find();
             if ($sumdata) {
                 $sumdata = $sumdata->toArray();
             }
             // 效率值 公式：[(得分+篮板+助攻+抢断+封盖)-(出手次数-命中次数)-(罚球次数-罚球命中次数)-失误次数]/球员上场比赛的场次
             // 出手次数
-            $sumFga = $sumdata['fga']+$sumdata['3pfga'];
+            $sumFga = $sumdata['fga']+$sumdata['threepfga'];
             // 命中次数
-            $sumFg = $sumdata['fg']+$sumdata['3pfg'];
+            $sumFg = $sumdata['fg']+$sumdata['threepfg'];
             if ($matchNumber) {
                 $efficiency = (($sumdata['pts']+$sumdata['reb']+$sumdata['ast']+$sumdata['stl']+$sumdata['blk']) - ($sumFga-$sumFg) - ($sumdata['fta']-$sumdata['ft']) - $sumdata['turnover']) / $matchNumber ;
             }
@@ -123,7 +123,7 @@ class Matchdata
                 $recordMembers[$k]['team'] = $data['team'];
                 $recordMembers[$k]['status'] = 1;
                 // 球员得分和
-                $recordMembers[$k]['pts'] = 2*$member['fg']+3*$member['3pfg']+1*$member['ft'];
+                $recordMembers[$k]['pts'] = 2*$member['fg']+3*$member['threepfg']+1*$member['ft'];
                 // 查询有无已有数据记录
                 $memberMatchStatisticsInfo = $model->where([
                     'match_id' => $data['match_id'],
