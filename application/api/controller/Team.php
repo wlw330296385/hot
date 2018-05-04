3bdee66b5ad8cb263e4c2cf659010fe1eb226994
weilin <704671079@qq.com>
2018-05-04 18:08:54 +0800

    球队最新比赛、活动 过期数据不显示（比赛的比赛时间，活动的结束时间小于当前算“过期”)

--- application/api/controller/Team.php
+++ application/api/controller/Team.php
@@ -1331,6 +1331,9 @@ class Team extends Base
 
             // 默认查询上架比赛(status=1)
             $map['status'] = input('param.status', 1);
+            // 比赛时间小于当前时间：过期比赛
+            $map['match_record.match_time'] = ['gt', time()];
+
             // 查询条件组合end
             // 未完成比赛优先
             $orderby = ['is_finished' => 'asc', 'id' => 'desc'];
@@ -1402,7 +1405,8 @@ class Team extends Base
                 }
                 unset($map['member_id']);
             }
-
+            // 比赛时间小于当前时间：过期比赛
+            $map['match_record.match_time'] = ['gt', time()];
             // 查询条件组合end
             if (input('?param.page')) {
                 unset($map['page']);
@@ -1477,8 +1481,8 @@ class Team extends Base
 
             // 默认查询上架比赛(status=1)
             $map['status'] = input('param.staus', 1);
-            // 默认查询未完成比赛(is_finished=-1)
-            //$map['is_finished'] = input('param.is_finished', -1);
+            // 比赛时间小于当前时间：过期比赛
+            $map['match_record.match_time'] = ['gt', time()];
             // 查询条件组合end
 
             if (input('?param.page')) {
@@ -1591,6 +1595,8 @@ class Team extends Base
             }
             // 默认查询上架活动(status=1)
             $map['status'] = input('param.staus', 1);
+            // 活动结束时间小于当前时间: 当过期
+            $map['end_time'] = ['gt', time()];
             // 查询条件组合end
             // 未完成的记录优先
             $orderby = ['is_finished' => 'asc', 'id' => 'desc'];
@@ -1639,6 +1645,8 @@ class Team extends Base
             }
             // 默认查询上架活动(status=1)
             $map['status'] = input('param.staus', 1);
+            // 活动结束时间小于当前时间: 当过期
+            $map['end_time'] = ['gt', time()];
             // 查询条件组合end
             if (input('?param.page')) {
                 unset($map['page']);
@@ -1690,7 +1698,8 @@ class Team extends Base
                 }
                 unset($map['member_id']);
             }
-
+            // 活动结束时间小于当前时间: 当过期
+            $map['end_time'] = ['gt', time()];
             // 查询条件组合end
             if (input('?param.page')) {
                 unset($map['page']);
