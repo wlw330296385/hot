f0ef760671f8b5bfd8dc3b1d0aa71006eea2ff23
1303274598@qq.com <1303274598@qq.com>
2018-05-24 15:23:03 +0800

    fixed

--- application/admin/controller/base/Base.php
+++ application/admin/controller/base/Base.php
@@ -2,47 +2,23 @@
 // admin模块 控制器基类
 namespace app\admin\controller\base;
 
-use app\service\SystemService;
+
 use think\Controller;
 use app\service\AuthService;
-use think\Cookie;
-use app\model\Camp;
 class Base extends Controller {
-    public $cur_camp;
     public $site;
     public $AuthService;
     public $admin;
     public function _initialize() {
         $this->AuthService = new AuthService;
-        // 获取平台数据
-        $SystemS = new SystemService();
-        $site = $SystemS->getSite();
-        $this->site = $site;
-        $this->cur_camp = $this->getCurCamp();
-        $this->assign('site', $site);
-        $this->assign('admin', session('admin') );
-
-
-    }
-
-    // 获取当前查看训练营
-    public function getCurCamp() {
-        if ( Cookie::has('camp_id', 'curcamp_') ) {
-            $res = [
-                'camp_id' => Cookie::get('camp_id', 'curcamp_'),
-                'camp' => Cookie::get('camp', 'curcamp_')
-            ];
-            return $res;
-        } else {
-            $res = [
-                'camp_id' => 9,
-                'camp' =>'大热训练营', 
-            ];
-            return $res;
+        // 检查控制台登录
+        if ( !$this->AuthService->islogin() ) {
+            //$this->error('请登录后操作', url('Login/index'));
+            $this->redirect('Login/index');
+        }else{
+            $this->admin = session('admin');
+            $this->assign('admin',$this->admin);
         }
     }
 
-    protected function record($do){
-        $this->AuthService->record($do);
-    }
 }
