1b6d876e7cb685ec00bc98210a4d321d717028c3
1303274598@qq.com <1303274598@qq.com>
2018-05-25 10:44:41 +0800

    2~

---
+++ application/model/GroupMember.php
@@ -0,0 +1,15 @@
+<?php 
+namespace app\model;
+use think\Model;
+use traits\model\SoftDelete;
+class GroupMember extends Model {
+    use SoftDelete;
+    protected $deleteTime = 'delete_time';
+	protected $autoWriteTimestamp = true;
+    // protected $readonly = [
+    //                         'create_time',
+    //                         'status',
+    //                         'type',
+    //    
+
+}
