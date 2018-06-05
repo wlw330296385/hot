20d222986a53b1c34ecf7df644e0a2ea724cf309
wayen <zwr_0320@163.com>
2018-06-05 10:20:02 +0800

    活动登记添加一个页面加图文简介

--- application/index/controller/Apps.php
+++ application/index/controller/Apps.php
@@ -27,10 +27,24 @@ class Apps extends Base
     }
 
 
-	public function appsForm1(){
+	// public function appsForm1(){
 
     	
-    	return view('apps/appssForm1');
+    // 	return view('apps/appssForm1');
 
+	// }
+	
+	public function appsForm1(){
+		$member_id = session('memberInfo.id');
+    	$event_id = input('param.event_id');
+    	$memberInfo = db('member')->where(['id'=>$member_id])->find();
+    	$cert = db('cert')->where(['member_id'=>$member_id,'cert_type'=>1])->find();
+    	$eventInfo = db('event')->where(['id'=>$event_id])->find();
+
+
+    	$this->assign('memberInfo',$memberInfo);
+    	$this->assign('cert',$cert);
+    	$this->assign('eventInfo',$eventInfo);
+   		 return view('apps/appsForm1');
     }
 }
