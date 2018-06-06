da1015e1a0b41f12643dfb3cd475562e28959377
1303274598@qq.com <1303274598@qq.com>
2018-06-06 17:08:12 +0800

    1

--- application/api/controller/GroupApply.php
+++ application/api/controller/GroupApply.php
@@ -64,9 +64,11 @@ class GroupApply extends Base{
             $data = input('post.');
             $group_apply_id = input('param.group_apply_id');
             $member_id = input('param.member_id');
+            $type = input('param.type');
+            //同意操作
             if($group_apply_id){
                 $result = $this->GroupApplyService->updateGroupApply($data,$group_apply_id);
-            }elseif ($member_id<>) {
+            }elseif ($member_id<> $this->memberInfo['id']) {
                 $data['member_id'] = $this->memberInfo['id'];
                 $data['member'] = $this->memberInfo['member'];
                 $data['avatar'] = $this->memberInfo['avatar'];
