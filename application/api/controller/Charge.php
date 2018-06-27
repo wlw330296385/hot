2ed1aca696c9192e9cdd53bf3e08abf756a2e744
1303274598@qq.com <1303274598@qq.com>
2018-06-27 15:51:40 +0800

    1

--- application/api/controller/Charge.php
+++ application/api/controller/Charge.php
@@ -17,43 +17,74 @@ class Charge extends Frontend{
 	        $type = input('param.type',1);
 	        if($charge >=1){
 	        	if($member_id<1){
-	        		return json(['code'=>100,'msg'=>'未注册平台会员,充值未到账,请联系平台客服']);
+	        		return json(['code'=>100,'msg'=>'未注册平台会员,购买未到账,请联系平台客服']);
 	        	}
 	        	if(!$charge_order){
-	        		return json(['code'=>100,'msg'=>'充值单号错误']);
+	        		return json(['code'=>100,'msg'=>'购买单号错误']);
 	        	}
-	        	$Charge = new \app\model\Charge;
-	        	$result = $Charge->save([
-	        		'member'		=>$member,
-	        		'member_id'		=>$member_id,
-	        		'avatar'		=>$avatar,
-	        		'charge'		=>$charge,
-	        		'charge_order'	=>$charge_order,
-	        		'status'		=>1
-	        	]);
 	        	if($result){
 	        		switch($type){
 	        			case 1:
+		        			$Charge = new \app\model\Charge;
+				        	$result = $Charge->save([
+				        		'member'		=>$member,
+				        		'member_id'		=>$member_id,
+				        		'avatar'		=>$avatar,
+				        		'charge'		=>$charge,
+				        		'charge_order'	=>$charge_order,
+				        		'status'		=>1
+				        	]);
 	        				db('member')->where(['id'=>$member_id])->inc('balance',$charge)->update();
 	        				session('memberInfo.balance',($this->memberInfo['balance']+$charge));
 	        			break;
 	        			case 2:
+		        			$Charge = new \app\model\Charge;
+				        	$result = $Charge->save([
+				        		'member'		=>$member,
+				        		'member_id'		=>$member_id,
+				        		'avatar'		=>$avatar,
+				        		'charge'		=>$charge,
+				        		'charge_order'	=>$charge_order,
+				        		'status'		=>1
+				        	]);
 	        				db('member')->where(['id'=>$member_id])->inc('hot_coin',$charge)->update();
+	        				$Hotcoin = new \app\model\Hotcoin;
+	        				$Hotcoin->save([
+				        		'member'		=>$member,
+				        		'member_id'		=>$member_id,
+				        		'avatar'		=>$avatar,
+				        		'f_id'			=>$Charge->id,
+				        		'hot_coin'		=>$charge,
+				        		'type'			=>1,
+				        		'status'		=>1
+				        	]);
 	        				session('memberInfo.hot_coin',($this->memberInfo['hot_coin']+$charge));
 	        			break;
 	        			case 3:
+		        			$Charge = new \app\model\Charge;
+				        	$result = $Charge->save([
+				        		'member'		=>$member,
+				        		'member_id'		=>$member_id,
+				        		'avatar'		=>$avatar,
+				        		'charge'		=>$charge,
+				        		'charge_order'	=>$charge_order,
+				        		'status'		=>1
+				        	]);
 	        				db('camp')->where(['id'=>$camp_id])->inc('balance',$charge)->update();
 	        			break;
+	        			case -1:
+	        				return json(['code'=>200,'msg'=>'操作成功']);
+	        			break;
 	        			default:   
-	        			return json(['code'=>100,'msg'=>'请指定充值的类型']); 
+	        			return json(['code'=>100,'msg'=>'请指定购买的类型']); 
 	        		}
 	        		
-	        		return json(['code'=>200,'msg'=>'充值成功']);
+	        		return json(['code'=>200,'msg'=>'购买成功']);
 	        	}else{
-	        		return json(['code'=>100,'msg'=>'充值失败,请联系平台客服通过充值记录保证您自身的权益']);
+	        		return json(['code'=>100,'msg'=>'购买失败,请联系平台客服通过充值记录保证您自身的权益']);
 	        	}
 	        }else{
-	        	return json(['code'=>100,'msg'=>'充值金额必须大于1']);
+	        	return json(['code'=>100,'msg'=>'购买金额必须大于1']);
 	        }    		
     	}catch(Exception $e){
     		return json(['code'=>100,'msg'=>$e->getMessage()]);
--- application/model/Hotcoin.php
+++ application/model/Hotcoin.php
@@ -9,4 +9,9 @@ class Hotcoin extends Model {
     protected $autoWriteTimestamp = true;
     use SoftDelete;
     protected $deleteTime = 'delete_time';
+
+    public function getTypeAttr($value){
+		$list = [1=>'课程订单收入',2=>'活动订单收入',3=>'课时收入',4=>'充值',5=>'退款收入',6=>'剔除学生收入'];
+		return $list[$value];
+	}	
 }
