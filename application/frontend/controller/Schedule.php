22c546303ea98fb8ccc339257d5bfce44a4ef24b
1303274598@qq.com <1303274598@qq.com>
2018-07-31 18:01:15 +0800

    fixed

--- application/frontend/controller/Schedule.php
+++ application/frontend/controller/Schedule.php
@@ -64,12 +64,13 @@ class Schedule extends Base
     	$lessonMember = db('lesson_member')->where(['student_id'=>$student_id,'camp_id'=>$camp_id,'status'=>1])->where('delete_time',null)->find();
         // 课时统计
         $scheuldeIDS = db('schedule')->where(['grade_id'=>['in',$gradeIDS],'camp_id'=>$camp_id])->where('delete_time',null)->column('id');
-        $scheduleCount = $this->ScheduleService->countScheduleMembers(['camp_id' => $camp_id,'schedule_id'=>['in',$scheuldeIDS],'user_id'=>$student_id,'status'=>1]);
-        $totalCount = db('schedule_member')->where(['camp_id' => $camp_id,'schedule_id'=>['in',$scheuldeIDS],'user_id'=>$student_id,'status'=>1])->count();
+        $scheduleCount = $this->ScheduleService->countScheduleMembers(['camp_id' => $camp_id,'user_id'=>$student_id]);
+
+        $totalCount = db('schedule_member')->where(['camp_id' => $camp_id,'schedule_id'=>['in',$scheuldeIDS],'user_id'=>$student_id,'status'=>-1])->count();
         $this->assign('gradeIDS',json_encode($gradeIDS));
         $this->assign('lessonMember',$lessonMember);
     	$this->assign('scheduleCount',$scheduleCount);
-        
+
         $this->assign('totalCount',$totalCount);
   		$this->assign('gradeList',$gradeList);
   		$this->assign('studentList',$studentList);
