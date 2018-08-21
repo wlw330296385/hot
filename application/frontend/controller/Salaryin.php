2ecd1f3ed44c94eb8bacda3d594752d1a0976016
1303274598@qq.com <1303274598@qq.com>
2018-08-21 18:23:58 +0800

    页面输出教练数据

--- application/frontend/controller/Salaryin.php
+++ application/frontend/controller/Salaryin.php
@@ -19,13 +19,18 @@ class Salaryin extends Base{
         $y = date('Y',time());
         $m = date('m',time());
         // 教练总数和教练列表
-        $coachList = db('camp_member')->where(['type'=>['in',[2,4]],'status'=>1,'camp_id'=>$camp_id])->select();
+        $coachList = db('camp_member')->field('camp_member.member,camp_member.member_id,coach.id,coach.coach')->join('coach','coach.member_id = camp_member.member_id')->where(['camp_member.type'=>['in',[2,4]],'camp_member.status'=>1,'camp_member.camp_id'=>$camp_id])->order('camp_member.id desc')->select();
         $coachCount = count($coachList);
 
         $coachIDs = [];
+        $memberIDs = [];
         foreach ($coachList as $key => $value) {
-            $coachIDs[] = $value['member_id'];
+            $coachIDs[] = $value['id'];
+            $memberIDs[] = $value['member_id'];
+            $coachList[$key]['ss'] = 0;
+            $coachList[$key]['s'] = 0;
         }
+
         // 工资总额
         $countSalaryin = $this->SalaryInService->countSalaryin(['camp_id'=>$camp_id,'type'=>1]);
         // 工资列表,由于根据结算时间和上课时间得出的结果不同,因此不可联表查
@@ -34,25 +39,52 @@ class Salaryin extends Base{
         // 获取结算时间
         $salaryList = db('salary_in')
                     ->field('realname,sum(push_salary+salary) as s_salary,member_id')
-                    ->where(['camp_id'=>$camp_id,'type'=>1,'create_time'=>['between',[$between['start'],$between['end']]],'member_id'=>['in',$coachIDs]])
+                    ->where(['camp_id'=>$camp_id,'type'=>1,'create_time'=>['between',[$between['start'],$between['end']]],'member_id'=>['in',$memberIDs]])
                     ->group('member_id')
                     ->select();
+        foreach ($coachList as $k => $val) {
+  
+            foreach ($salaryList as $key => $value) {
 
-        // dump($salaryList);
+                if($value['member_id'] == $val['member_id']){
+                     
+                    $coachList[$k]['ss']+=$value['s_salary'];
+                }
+            }
+        }
 
         // 获取课时工资
         $sacheduleSalaryList = db('schedule')
                 ->field('coach_salary,assistant_salary,salary_base,students,coach_id,assistant_id')
                 ->where(['status'=>1,'lesson_time'=>['between',[$between['start'],$between['end']]],'camp_id'=>$camp_id]) 
                 ->select();
-                // dump($sacheduleSalaryList);die;
+
         foreach ($sacheduleSalaryList as $key => $value) {
-                
-             }     
+            if($value['assistant_id']){
+                $sacheduleSalaryList[$key]['a_ids'] = unserialize($value['assistant_id']);
+            }else{
+                 $sacheduleSalaryList[$key]['a_ids'] = [];
+            }
+        }
+
+
+        foreach ($coachList as $k => $val) {
+            foreach ($sacheduleSalaryList as $key => $value) {
+                if($val['member_id'] == $value['coach_id']){
+                    $coachList[$k]['s']+=$value['coach_salary']+$value['salary_base']*$value['students'];
+                }   
+
+                if(in_array($val['member_id'],$value['a_ids'])){
+                    $coachList[$k]['s']+=$value['assistant_salary']+$value['salary_base']*$value['students'];
+                }
+            }  
+        }   
+        
         $this->assign('y',$y); 
         $this->assign('m',$m);
         $this->assign('coachCount',$coachCount);
         $this->assign('camp_id', $camp_id);
+        $this->assign('coachList', $coachList);
         return view('Salaryin/salaryOfCamp');
     }
 
