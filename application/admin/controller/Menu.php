af1e35b23e7e890ea0cae88dd60c4a765e4359e3
1303274598@qq.com <1303274598@qq.com>
2018-08-23 17:15:39 +0800

    admin菜单管理功能

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -11,8 +11,7 @@ class Menu extends Backend{
 
 
 	public function menuList(){
-		// $list = $this->MenuModel->select();
-		$list = db('admin_menu')->where(['status'=>1])->select();
+		$list = db('admin_menu')->select();
 		$menuList = getTree($list);
 		
 		$this->assign('menuList', $menuList);
@@ -23,25 +22,31 @@ class Menu extends Backend{
 
 		if(request()->isPost()){
 			$data = input('post.');
-			$data['password'] = $data['repassword'] = $data['system_remarks'] = rand(100000,999999);
-			$result = $this->MenuService->saveMenuInfo($data);
-			if($result['code'] == 100){
-				echo '<script type="text/javascript">alert("'.$result["msg"].'")</script>';
+			
+			$AdminMenu = new \app\admin\model\AdminMenu;
+			$result = $AdminMenu->save($data);
+
+			if($result){
+				$this->success('操作成功');
 			}else{
-				// 判断是否有添加学生
-				if($data['student']){
-					$data['menu_id'] = $result['data'];
-					$Student = new \app\model\Student;
-					$Student->save($data);
-				}
-				echo '<script type="text/javascript">alert("'.$result["msg"].'")</script>';
+				$this->error('操作失败');
 			}
 		}
-
-		return	$this->fetch('Menu/createMenu');
 	}
 
 
+	public function updateMenu(){
+		$id = input('param.id');
+		$data = input('post.');
+		$AdminMenu = new \app\admin\model\AdminMenu;
+		$result = $AdminMenu->save($data,['id'=>$id]);
+
+		if($result){
+			$this->success('操作成功');
+		}else{
+			$this->error('操作失败');
+		}
+	}
 
 
 	
