30b195fb1b2088fdd0247909d2443fdea3033e2c
1303274598@qq.com <1303274598@qq.com>
2018-08-23 15:12:33 +0800

    admin统计

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -13,10 +13,16 @@ class Index extends Backend {
     	$site = $this->site;
     	$memberCount = db('member')->where('delete_time',null)->count();
     	$campCount = db('camp')->where('delete_time',null)->count();
-    	$coachCount = db('coach')->where('delete_time',null)->count();
+    	$coachCount1 = db('coach')->where(['status'=>1])->where('delete_time',null)->count();
+        $coachCount2 = db('coach')->where(['status'=>['<>',1]])->where('delete_time',null)->count();
     	$courtCount = db('court')->where('delete_time',null)->count();
 
-
+        $lessonCount = db('lesson')->where(['status'=>1])->where('delete_time',null)->count();
+        $eventCount = db('event')->where(['status'=>1])->where('delete_time',null)->count();
+        $gradeCount = db('grade')->where(['status'=>1])->where('delete_time',null)->count();
+        $studentCount = db('student')->where(['status'=>1])->where('delete_time',null)->count();
+        $scheduleCount = db('schedule')->where(['status'=>1])->where('delete_time',null)->count();
+        $refereeCount = db('referee')->where(['status'=>1])->where('delete_time',null)->count();
         // 每日定时任务列表
         // $sql = db('crontab_record')->order('create_time desc')->select(false);//不支持mariaDB
         // $crontabList = Db::query("select * from ($sql) as a group by crontab");//不支持mariaDB
@@ -26,8 +32,16 @@ class Index extends Backend {
     	$this->assign('memberCount',$memberCount);
     	$this->assign('campCount',$campCount);
         $this->assign('crontabList',$crontabList);
-    	$this->assign('coachCount',$coachCount);
-    	$this->assign('courtCount',$courtCount);
+    	$this->assign('coachCount1',$coachCount1);
+        $this->assign('coachCount2',$coachCount2);
+        $this->assign('courtCount',$courtCount);
+
+        $this->assign('lessonCount',$lessonCount);
+        $this->assign('eventCount',$eventCount);
+    	$this->assign('gradeCount',$gradeCount);
+        $this->assign('studentCount',$studentCount);
+        $this->assign('scheduleCount',$scheduleCount);
+        $this->assign('refereeCount',$refereeCount);
         return view();
     }
 }
