70a6c0fbda3ec9f9ee9c389712380b3f1a158e90
1303274598@qq.com <1303274598@qq.com>
2018-09-17 16:46:09 +0800

    address删掉

--- application/management/controller/Bankcard.php
+++ application/management/controller/Bankcard.php
@@ -2,7 +2,7 @@
 namespace app\management\controller;
 use app\management\controller\Camp;
 // 按课时结算的训练营财务页面
-class Coach extends Camp{
+class Bankcard extends Camp{
 	public function _initialize(){
 		
 		parent::_initialize();
@@ -15,8 +15,10 @@ class Coach extends Camp{
 
 	// 添加银行卡
 	public function createBankcard(){
-		
 
+		$campBankcard = [];
+
+		$this->assign('campBankcard',$campBankcard);
 		return $this->fetch();
 	}
 }
--- application/management/controller/Grade.php
+++ application/management/controller/Grade.php
@@ -24,16 +24,6 @@ class Grade extends Backend{
 
             $data['member_id'] = $this->memberInfo['id'];
             $data['member'] = $this->memberInfo['member'];
-            if($data['address']){
-                $address = explode(' ', $data['address']);
-                $data['province'] = $address[0];
-                $data['city'] = $address[1];
-                if($address[2]){
-                    $data['area'] = $address[2];
-                }else{
-                    $data['area'] = $address[1];
-                }             
-            }
             $GradeService = new \app\service\GradeService;
             $result = $GradeService->createGrade($data);
             if ($result['code'] == 200) {
