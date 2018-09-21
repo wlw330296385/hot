69aa8f43e7beaf605fd48618051890a3fcf50b48
wayen <zwr_0320@163.com>
2018-09-21 16:06:00 +0800

    controller/match.php添加获取当前会员的联赛人员关系数据变量，联赛管理员以下职位只能查看章程，管理员以上可编辑章程的判断

--- application/keeper/controller/Match.php
+++ application/keeper/controller/Match.php
@@ -346,6 +346,18 @@ class Match extends Base {
     }
      // 联赛章程编辑
      public function regulationofleague() {
+         // 获取当前会员的联赛人员关系数据
+        $leagueS = new LeagueService();
+        $matchMemberInfo = $leagueS->getMatchMember([
+            'match_id' => $this->league_id,
+            'member_id' => $this->memberInfo['id'],
+            'status' => 1
+        ]);
+        if (!$matchMemberInfo) {
+            $this->error(__lang('MSG_403'));
+        }
+
+        $this->assign('matchMemberInfo', $matchMemberInfo);
         return view('Match/regulation/regulationOfLeague');
     }
 
