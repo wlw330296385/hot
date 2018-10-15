65c560f0025c4a51d8ddc8f10171e45e7c3878d4
1303274598@qq.com <1303274598@qq.com>
2018-10-15 16:45:56 +0800

    邀请家人

--- application/api/controller/Family.php
+++ application/api/controller/Family.php
@@ -34,5 +34,36 @@ class Family extends Base{
             return json(['code'=>100,'msg'=>$e->getMessage()]);
         }
     }
+
+    //新增一个邀请
+    public function createFamilyApi(){
+        try {
+            $map = ['member_id'=>$this->memberInfo['id']];
+            $data = input('post.');
+            $telephone = input('param.telephone');
+            if(!$telephone){
+                return json(['code'=>100,'msg'=>'邀请人电话号码必须']);
+            }
+            $map['telephone'] = $telephone;
+            $result = $this->FamilyModel->where($map)->find();
+            if($result){
+                if($result['status']<>-1){
+                    $res = $this->FamilyModel->save(['status'=>-1],$map);
+                }else{
+                    return json(['code'=>100,'msg'=>'重复邀请']); 
+                }
+            }
+            $data['avatar'] = $this->memberInfo['avatar'];
+            $data['member_id'] = $this->memberInfo['id'];
+            $res = $this->FamilyModel->save($data);
+            if($res){
+                return json(['code'=>100,'msg'=>'邀请成功','data'=>$this->FamilyModel->id]);
+            }else{
+                return json(['code'=>100,'msg'=>'邀请失败']);
+            }
+        } catch (Exception $e) {
+            return json(['code'=>100,'msg'=>$e->getMessage()]);
+        }
+    }
 }
 
