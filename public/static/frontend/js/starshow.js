2ae5b6c703c960e1298c8aa3e09e06ef7371ca7c
wayen <185918240@qq.com>
2018-10-27 00:05:24 +0800

    工作人员出场数据无数据提交判断添加；裁判评分赋值显示完善。

--- public/static/frontend/js/starshow.js
+++ public/static/frontend/js/starshow.js
@@ -24,3 +24,87 @@ function starShow(star) {
         return star5;
     }
 }
+
+
+var star01 = '<i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i>';
+var star02 = '<i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i>';
+var star03 = '<i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star"></i><i class="mui-icon mui-icon-star"></i>';
+var star04 = '<i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star"></i>';
+var star05 = '<i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i><i class="mui-icon mui-icon-star-filled"></i>';
+
+// 星级显示
+function fairnessStar(star) {
+    if (star == 1) {
+        $(".fairnessCont .icons").empty().append(star01);
+        return star01;
+    } else if (star == 2) {
+        $(".fairnessCont .icons").empty().append(star02);
+        return star02;
+    } else if (star == 3) {
+        $(".fairnessCont .icons").empty().append(star03);
+        return star03;
+    } else if (star == 4) {
+        $(".fairnessCont .icons").empty().append(star04);
+        return star04;
+    } else if (star == 5) {
+        $(".fairnessCont .icons").empty().append(star05);
+        return star05;
+    }
+}
+
+function punctualityStar(star) {
+    if (star == 1) {
+        $(".punctualityCont .icons").empty().append(star01);
+        return star01;
+    } else if (star == 2) {
+        $(".punctualityCont .icons").empty().append(star02);
+        return star02;
+    } else if (star == 3) {
+        $(".punctualityCont .icons").empty().append(star03);
+        return star03;
+    } else if (star == 4) {
+        $(".punctualityCont .icons").empty().append(star04);
+        return star04;
+    } else if (star == 5) {
+        $(".punctualityCont .icons").empty().append(star05);
+        return star05;
+    }
+}
+
+function professionStar(star) {
+    if (star == 1) {
+        $(".professionCont .icons").empty().append(star01);
+        return star01;
+    } else if (star == 2) {
+        $(".professionCont .icons").empty().append(star02);
+        return star02;
+    } else if (star == 3) {
+        $(".professionCont .icons").empty().append(star03);
+        return star03;
+    } else if (star == 4) {
+        $(".professionCont .icons").empty().append(star04);
+        return star04;
+    } else if (star == 5) {
+        $(".professionCont .icons").empty().append(star05);
+        return star05;
+    }
+}
+
+function attitudeStar(star) {
+    if (star == 1) {
+        $(".attitudeCont .icons").empty().append(star01);
+        return star01;
+    } else if (star == 2) {
+        $(".attitudeCont .icons").empty().append(star02);
+        return star02;
+    } else if (star == 3) {
+        $(".attitudeCont .icons").empty().append(star03);
+        return star03;
+    } else if (star == 4) {
+        $(".attitudeCont .icons").empty().append(star04);
+        return star04;
+    } else if (star == 5) {
+        $(".attitudeCont .icons").empty().append(star05);
+        return star05;
+    }
+}
