a8627442aea72b8e7eb90557e515899b570f53c6
wayen <zwr_0320@163.com>
2018-11-01 17:13:48 +0800

    联赛荣誉内外部列表排版样式修改，添加删除荣誉前端操作；联赛已完成不显示申请成为工作人员入口

--- application/keeper/controller/Match.php
+++ application/keeper/controller/Match.php
@@ -337,7 +337,30 @@ class Match extends Base {
          if (!$matchmember || $matchmember['status'] != 1) {
              $btnApplyWorkerShow = 1;
          }
-         
+
+         // 联赛可报名状态标识：
+        // 1 当前时间在报名开始时间与报名结束时间
+        // 2 联赛报名状态字段apply_status：1可报名|2结束报名
+        // 3 联赛无赛程记录
+        $cansignup = 1;
+        $nowtime = time();
+        if ( $nowtime < $this->leagueInfo['reg_start_timestamp'] ) {
+            // 等待报名（未到联赛报名时间）
+            $cansignup = -1;
+        } else if ($nowtime >= $this->leagueInfo['reg_start_timestamp'] && 
+            $nowtime <= $this->leagueInfo['reg_end_timestamp']) {
+            // 可报名
+            $cansignup = 0;
+        } else if ( $nowtime > $this->leagueInfo['start_timestamp'] && 
+            $nowtime <= $this->leagueInfo['end_timestamp']) {
+            // 正在进行
+            $cansignup = 1;
+        } else if ( $nowtime > $this->leagueInfo['end_timestamp']) {
+            // 结束报名
+            $cansignup = 2;
+        }
+
+        $this->assign('cansignup', $cansignup);
          $this->assign('types', $types);
          $this->assign('btnApplyWorkerShow', $btnApplyWorkerShow);
          //////////////////////////end//////////////////////////
